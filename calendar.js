1cf1bce297bdbdd63086cc64ee1f2eec08f7c34b
SerG-Y <serg.yaschuk@gmail.com>
2015-06-07 00:40:39 +0300

    js

--- calendar.js
+++ calendar.js
@@ -1,9 +1,12 @@
 var date = new Date();
 var dayName = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 var monthName = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var dayForKey;
 var year;
 var month;
 var yearsInterval;
+var tdControl;
+var flag = false;
 
 window.onload = function(){
   getCurrentDay();
@@ -11,10 +14,13 @@ window.onload = function(){
   document.getElementById("text").onkeydown = function(e){ keyPressOnInput(e); };
   document.getElementById("text").onchange = function(){ getDateWithInput(); };
   document.getElementById("text").onclick = function(){ setPosCursor(); };
-  document.onclick = function(e){
-    if (e.currentTarget.activeElement.localName != "input")
-        parseLineInput();
-  }
+  document.getElementsByClassName("icon")[0].onclick = function(){
+    document.getElementsByClassName("calendar-container")[0].style.display = "block";
+   getDateWithInput(); 
+   flag = true; 
+ };
+  document.onkeydown = function(e){ controlKey(e);};
+  refreshControl();
   document.getElementById("left").onclick = function(){previous();};
   document.getElementById("right").onclick = function(){next();};
   document.getElementById("calendar-month-year").onclick = function(){getMonthCalendar();};
@@ -24,9 +30,323 @@ window.onload = function(){
     document.getElementById("right").onclick = function(){next();};
     document.getElementById("calendar-month-year").onclick = function(){getMonthCalendar();};
     getCurrentDay();
+    document.onkeydown = function(e){ controlKey(e);};
   }
 }
 
+///////
+function refreshControl(){
+	tdControl = document.querySelectorAll('.days, .key');
+}
+
+function currentPosition(){
+	var temp = document.querySelectorAll('.days, .key');
+	for (var i = 0; i <  temp.length; i++) {
+		 if (temp[i].className == "key")
+		 	return i;
+	};
+}
+
+function controlKey(e){
+  if (!flag)
+    return;
+	if (e.keyCode == 38)
+		keyUp();
+	else if (e.keyCode == 40)
+		keyDown();
+	else if (e.keyCode == 37)
+		keyLeft();
+	else if(e.keyCode == 39)
+		keyRight();
+	else if(e.keyCode == 13)
+  {
+    var d = document.getElementsByClassName("key")[0];
+    var dayTemp = parseInt(d.innerText);
+    if (dayTemp < 10)
+      dayTemp = "0" + dayTemp;
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = dayTemp + " " + tmp[1] + " " + tmp[2];
+		getMonthCalendar();
+    dayForKey = dayTemp;
+    document.onkeydown = function(e){ controlMonthKey(e);};
+  }
+}
+
+function keyUp(){
+	var currentIndex = currentPosition();
+	tdControl[currentIndex].className = "days";
+	if (currentIndex < 7)
+	{
+		previous();
+		tdControl[tdControl.length - (7 - currentIndex)].className = "key";
+	}
+	else
+		tdControl[currentIndex - 7].className = "key";
+
+}
+
+function keyDown(){
+	var currentIndex = currentPosition();
+	tdControl[currentIndex].className = "days";
+	if ((currentIndex + 7) > tdControl.length - 1)
+	{
+		var count = tdControl.length - 1;
+		next();
+		tdControl[6 - (count - currentIndex)].className = "key";
+	}
+	else
+		tdControl[currentIndex + 7].className = "key";
+}
+
+function keyLeft(){
+	var currentIndex = currentPosition();
+	tdControl[currentIndex].className = "days";
+	if (currentIndex == 0 ){
+		previous();
+		tdControl[tdControl.length - 1].className = "key";
+	}
+	else 
+		tdControl[currentIndex - 1].className = "key";
+}
+
+function keyRight(){
+	var currentIndex = currentPosition();
+	tdControl[currentIndex].className = "days";
+	if (currentIndex == (tdControl.length - 1))
+	{
+		next();
+		tdControl[0].className = "key";
+	}
+	else
+		tdControl[currentIndex + 1].className = "key";
+}
+
+function controlMonthKey(e)
+{
+  if (!flag)
+    return;
+	if (e.keyCode == 38)
+		keyUpOnMonth();
+	else if (e.keyCode == 40)
+		keyDownOnMonth();
+	else if (e.keyCode == 37)
+		keyLeftOnMonth();
+	else if(e.keyCode == 39)
+		keyRightOnMonth();
+	else if(e.keyCode == 13)
+	{ 
+    var currentIndex = currentMonth();
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + monthName[currentIndex] + " " + tmp[2];
+		getYearCalendar();
+    document.onkeydown = function(e){ controlYearKey(e);};
+	}
+}
+
+function currentMonth()
+{
+  tdControl = document.getElementsByTagName("td");
+  for (var i = 0; i < tdControl.length; i++) {
+    if (tdControl[i].className == "keyMonth")
+      {
+        return i;
+      }
+  }
+  return undefined;
+}
+
+function keyUpOnMonth()
+{
+  var currentIndex = currentMonth();
+  if (currentIndex == undefined)
+    tdControl[tdControl.length - 3].className = "keyMonth";
+  else if(currentIndex < 3)
+  {
+    year--;
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + tmp[1] + " " + year;
+    document.getElementById("calendar-month-year").innerHTML = year;
+    tdControl[currentIndex].className = "";
+    tdControl[9 + currentIndex].className = "keyMonth"
+  }
+  else
+  {
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex - 3].className = "keyMonth";
+  }
+}
+function keyDownOnMonth()
+{
+  var currentIndex = currentMonth();
+  if (currentIndex == undefined) 
+    tdControl[3].className = "keyMonth";
+  else if (currentIndex > 8)
+  {
+    year++;
+    tdControl[currentIndex].className = "";
+    tdControl[tdControl.length - 1 - currentIndex].className = "keyMonth";
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + tmp[1] + " " + year;
+    document.getElementById("calendar-month-year").innerHTML = year;
+  }
+  else
+  {
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex + 3].className = "keyMonth";
+  }
+}
+function keyLeftOnMonth()
+{
+  var currentIndex = currentMonth();
+  if (currentIndex != undefined && currentIndex != 0)
+  {
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex - 1].className = "keyMonth";
+  }
+  else if(currentIndex == undefined)
+    tdControl[tdControl.length - 1].className = "keyMonth";
+  else
+  {
+    tdControl[0].className = "";
+    tdControl[tdControl.length - 1].className = "keyMonth";
+    year--;
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + tmp[1] + " " + year;
+    document.getElementById("calendar-month-year").innerHTML = year;
+  } 
+}
+function keyRightOnMonth()
+{
+  var currentIndex = currentMonth();
+  if (currentIndex != undefined && currentIndex != tdControl.length - 1)
+  {
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex + 1].className = "keyMonth";
+  }
+  else if(currentIndex != tdControl.length - 1)
+    tdControl[0].className = "keyMonth";
+  else
+  {
+    tdControl[currentIndex].className = "";
+    tdControl[0].className = "keyMonth";
+    year++;
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + tmp[1] + " " + year;
+    document.getElementById("calendar-month-year").innerHTML = year;
+  }  
+}
+
+function controlYearKey(e){
+  if (!flag)
+    return;
+  if (e.keyCode == 38)
+    keyUpOnYear();
+  else if (e.keyCode == 40)
+    keyDownOnYear();
+  else if (e.keyCode == 37)
+    keyLeftOnYaer();
+  else if(e.keyCode == 39)
+    keyRightOnYear();
+  else if(e.keyCode == 13)
+  { 
+    var currentIndex = currentYear();
+    year = parseInt(tdControl[currentIndex].innerText);
+    var input = document.getElementById("text");
+    var tmp = input.value.split(' ');
+    input.value = tmp[0] + " " + tmp[1] + " " + tdControl[currentIndex].innerText;
+    document.getElementById("calendar-month-year").innerHTML = monthName[month] + " " + year;
+    getInfo(dayForKey, month, year);
+    document.getElementById("left").onclick = function(){previous();};
+    document.getElementById("right").onclick = function(){next();};
+    document.onkeydown = function(e){ controlKey(e);};
+  }
+}
+
+function currentYear()
+{
+  tdControl = document.getElementsByTagName("td");
+  for (var i = 0; i < tdControl.length; i++)
+    if (tdControl[i].className == "keyYear")
+      return i;
+  return undefined;
+}
+
+function keyUpOnYear()
+{
+  var currentIndex = currentYear();
+  if (currentIndex == undefined || currentIndex < 4)
+  {
+    getYearsTable(yearsInterval);
+    if (currentIndex == undefined)
+      tdControl[tdControl.length - 4].className = "keyYear";
+    else
+      tdControl[tdControl.length - 1 - (3 - currentIndex)].className = "keyYear";
+  }
+  else{
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex - 4].className = "keyYear";
+  }
+}
+
+function keyDownOnYear()
+{
+  var currentIndex = currentYear();
+  if (currentIndex > 11)
+  {
+    getYearsTable(yearsInterval + 30);
+    tdControl[4 - (tdControl.length - currentIndex)].className = "keyYear";
+  }
+  else{
+    if (currentIndex == undefined)
+      currentIndex = 0;
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex + 4].className = "keyYear";
+  }
+}
+
+function keyLeftOnYaer()
+{
+  var currentIndex = currentYear();
+  if (currentIndex == undefined || currentIndex == 0)
+  {
+    if (currentIndex != undefined)
+      tdControl[currentIndex].className = "";
+    getYearsTable(yearsInterval);
+    tdControl[tdControl.length - 1].className = "keyYear";
+
+  }
+  else{
+    tdControl[currentIndex].className = "";
+    tdControl[currentIndex - 1].className = "keyYear";
+  }
+  
+}
+
+function keyRightOnYear()
+{
+  var currentIndex = currentYear();
+  if (currentIndex == tdControl.length - 1)
+  {
+    tdControl[currentIndex].className = "";
+    getYearsTable(yearsInterval + 30);
+    tdControl[0].className = "keyYear";
+  }
+  else{
+    if (currentIndex == undefined)
+      tdControl[0].className = "keyYear";
+    else{
+      tdControl[currentIndex].className = "";
+      tdControl[currentIndex + 1].className = "keyYear";
+    }
+  } 
+}
+//////////
 function getCaretPosition (oField){
   var iCaretPos = 0;
   if (document.selection) {
@@ -45,22 +365,22 @@ function putPoint(){
   var cursorPosition = getCaretPosition(input);
   if (cursorPosition == 2){
     if (input.value[2] != undefined)
-      input.value[2] =  ".";
+      input.value[2] =  " ";
     else
-      input.value += ".";
+      input.value += " ";
   }
   if (cursorPosition == 5){
     if (input.value[5] != undefined)
-      input.value[5] =  ".";
+      input.value[5] =  " ";
     else
-      input.value += ".";
+      input.value += " ";
   }
   cursorPosition = getCaretPosition(input);
   if (cursorPosition == 1){
     var num = parseInt(input.value.substring(0,1));
     if (num > 3){
       var tmp = input.value.substring(2, input.value.length);
-      input.value = "0" + num + "." + tmp;
+      input.value = "0" + num + " " + tmp;
     }
   }
   cursorPosition = getCaretPosition(input);
@@ -69,14 +389,16 @@ function putPoint(){
     if (num > 1){
         var dayTemp = input.value.substring(0,3);
         var yearTemp = input.value.substring(5,9); 
-        input.value = dayTemp + "0" + num + "." + yearTemp;
+        input.value = dayTemp + "0" + num + " " + yearTemp;
       }
   }    
 }
 
 function keyPressOnInput(e){
   var input = document.getElementById("text");
+  var help = document.getElementById("help");
   if(e.keyCode == 8 ) {
+      e.preventDefault();
        input.oninput = null;
   }
   else{
@@ -84,36 +406,138 @@ function keyPressOnInput(e){
     if (cursorPosition <= 2 || cursorPosition <= 5){
       if (cursorPosition == 2){
         if (input.value[2] != undefined)
-          input.value[2] =  ".";
+          input.value[2] =  " ";
         else
-          input.value += ".";
+          input.value += " ";
       }
       if (cursorPosition == 5){
         if (input.value[5] != undefined)
-          input.value[5] =  ".";
+          input.value[5] =  " ";
         else
-          input.value += ".";
+          input.value += " ";
       }
       input.oninput = function(e){ putPoint(e); };
     }
   }
-  if (e.keyCode == 13 && input.value.length == 10){
-    getDateWithInput();
+
+  var curPos = getCaretPosition(input);
+  if (e.keyCode == 38 && (curPos >=0 && curPos <=2))
+  {
+    e.preventDefault();
+    var val = input.value.split(' ');
+    var day = parseInt(val[0]);
+    day++;
+    if(day < 10)
+      day = "0" + day;
+    if (day > 31)
+      return;
+    input.value = day + " " + val[1] + " " + val[2];
+    input.setSelectionRange(curPos, curPos); 
   }
+  curPos = getCaretPosition(input);
+  if (e.keyCode == 40 && (curPos >=0 && curPos <=2))
+  {
+    e.preventDefault();
+    var curPos = getCaretPosition(input);
+    var val = input.value.split(' ');
+    var day = parseInt(val[0]);
+    day--;
+    if(day < 10)
+      day = "0" + day;
+    if (day <= 0)
+      return;
+    input.value = day + " " + val[1] + " " + val[2]; 
+    input.setSelectionRange(curPos, curPos);
+  }
+  //inrease month
+  curPos = getCaretPosition(input);
+  if (e.keyCode == 38 && (curPos >=3 && curPos <=5))
+  {
+    e.preventDefault();
+    var val = input.value.split(' ');
+    month = parseInt(val[1]);
+    month++;
+    if(month < 10)
+      month = "0" + month;
+    if (month > 12)
+      return;
+    input.value = val[0] + " " + month + " " + val[2];
+    input.setSelectionRange(curPos, curPos);
+
+  }
+  curPos = getCaretPosition(input);
+  if (e.keyCode == 40 && (curPos >=3 && curPos <=5))
+  {
+    e.preventDefault();
+    var val = input.value.split(' ');
+    month = parseInt(val[1]);
+    month--;
+    if(month < 10)
+      month = "0" + month;
+    if (month < 1)
+      return;
+    input.value = val[0] + " " + month + " " + val[2];
+    input.setSelectionRange(curPos, curPos); 
+  }
+
+  //inrease year
+  curPos = getCaretPosition(input);
+  if (e.keyCode == 38 && (curPos >=6 && curPos <=10))
+  {
+    e.preventDefault();
+    var val = input.value.split(' ');
+    year = parseInt(val[2]);
+    year++;
+    if(year < 10)
+      year = "0" + year;
+    input.value = val[0] + " " + val[1] + " " + year;
+    input.setSelectionRange(curPos, curPos); 
+  }
+  curPos = getCaretPosition(input);
+  if (e.keyCode == 40 && (curPos >=6 && curPos <=10))
+  {
+    e.preventDefault();
+    var val = input.value.split(' ');
+    year = parseInt(val[2]);
+    year++;
+    if(year < 10)
+      year = "0" + year;
+    input.value = val[0] + " " + val[1] + " " + year;
+    input.setSelectionRange(curPos, curPos); 
+  }
+  curPos = getCaretPosition(input);
+  if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105))
+  {
+    e.preventDefault();
+    if (curPos == 2 || curPos == 5)
+      return;
+    
+    var val = String.fromCharCode(e.keyCode);
+    input.value = input.value.substring(0, curPos) + val + input.value.substring(curPos + val.length, input.value.length);
+    input.setSelectionRange(curPos + 1, curPos + 1);
+  }
+  curPos = getCaretPosition(input);
+  if((curPos >=3 && curPos <=5))
+  {
+    help.style.display = "block";
+    var val = input.value.split(' ');
+    var tmp = parseInt(val[1]);
+    help.innerText = monthName[tmp - 1];
+  }
+  else
+  {
+    help.style.display = "none";
+  }
+
 }
 
 function setPosCursor(){
+  flag = false;
   var input = document.getElementById("text");
   var cursorPosition = getCaretPosition(input);
   if (input.value.length > 10){
     parseToDate();
   }
-  if (cursorPosition == 0)
-    input.setSelectionRange(0, 2);
-  if (cursorPosition == 3)
-    input.setSelectionRange(3, 5);
-  if (cursorPosition == 6)
-    input.setSelectionRange(6, 10);
 }
 
 function getDateWithInput(){
@@ -158,7 +582,8 @@ function parseToDate(){
   var monthTemp = parseInt(monthName.indexOf(line[1])) + 1;
   if (monthTemp < 10)
     monthTemp = "0" + monthTemp;
-  input.value = line[0] + "." + monthTemp + "." + line[2];
+  input.value = line[0] + " " + monthTemp + " " + line[2];
+  input.maxLength = 10;
 }
 
 function getCurrentDay(){
@@ -169,6 +594,7 @@ function getCurrentDay(){
 }
 
 function getInfo(day, month, year){
+  document.onkeydown = function(e){ controlKey(e);};
   var table = document.getElementsByTagName("table")[0];
   if (table != undefined)
     table.parentNode.removeChild(table);
@@ -182,6 +608,8 @@ function getInfo(day, month, year){
   var input = document.getElementById("text");
   if (day == undefined)
     day = "01";
+  else if (day < 10)
+    day = "0" + day;
   input.value = day + " " + monthName[month] + " " + year;
 }
 
@@ -203,13 +631,9 @@ function getCalendar(dayNum, nDays, nPrevDays, currentDay){
     for (var j = 0; j <= 6; j++ , counter++) {
       var td = document.createElement('td');
       if (counter == dayNum || counter > dayNum){
-        if (countDays == date.getDate() && year == date.getFullYear() && month == date.getMonth()
-         || ( currentDay != undefined && countDays == currentDay))
+        if ( currentDay != undefined && countDays == currentDay)
         {
-          if (currentDay != undefined && countDays == currentDay && currentDay != date.getDate())
-            td.className = "current-day";
-          else
-            td.className = "today";
+          td.className = "key";
           td.innerHTML = countDays;
           countDays++;
         }
@@ -237,6 +661,7 @@ function getCalendar(dayNum, nDays, nPrevDays, currentDay){
   table.classList.add("calendar-number");
   document.getElementById("calendar-dates").appendChild(table);
   refreshClick();
+  refreshControl();
 }
 
 function previous(){
@@ -288,6 +713,7 @@ function refreshClick(){
 }
 
 function getMonthCalendar(){
+  document.onkeydown = function(e){ controlMonthKey(e);};
   document.getElementById("left").onclick = function () {
     year--;
     document.getElementById("calendar-month-year").innerHTML = year;
@@ -336,6 +762,7 @@ function clickOnMonth(){
 }
 
 function getYearCalendar(){
+  document.onkeydown = function(e){ controlYearKey(e);};
   document.getElementById("left").onclick = function () {
     getYearsTable(yearsInterval);}
   document.getElementById("right").onclick = function () {
